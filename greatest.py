be4fae66b452adcc3eed139d9a563495ab5bcdcb
karishmabai06 <karishmabai2007@gmail.com>
2026-09-17 09:54:40 +0530

    changes

--- greatest.py
+++ greatest.py
@@ -25,4 +25,4 @@ def index():
     return render_template("index.html", result=result, num1=num1, num2=num2, num3=num3)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
